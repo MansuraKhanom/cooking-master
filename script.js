6155d2fe01bdc099407898954f4941ee21a2e7cb
Mansura Khanom <mansura.ara@gmail.com>
2021-02-07 20:00:32 -0700

    search by name error handling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Reading the seach input and showing them on the console bar
 const searchBtn = document.getElementById("searchBtn");
+
 searchBtn.addEventListener("click", function () {
     const searchInput = document.getElementById("search").value;
     const foodName = document.getElementById("food-name");
@@ -7,12 +8,27 @@ searchBtn.addEventListener("click", function () {
     // Eleminating null string value
     if (searchInput !== "") {
         const fetchData = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`).then(
-            res => res.json()
+            res => {
+                // Response validation and showing error message
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw "No results found!";
+                }
+            },
+            rej => { throw "No results found!"; }
         ).then(
             data => {
-                foodName.innerText = data.meals[0].strMeal;
-            }
-        )
+                // data validation and showing error message
+                if (data.meals !== null) {
+                    // console.log(data);
+                    foodName.innerText = data.meals[0].strMeal;
+                } else {
+                    throw "No results found!";
+                }
+            },
+            rej => { throw "No results found!"; }
+        ).catch(err => foodName.innerText = err)
 
     } else {
         foodName.innerText = "Please enter a valid input!";
