3bf098244be97549bf24137622268d90fbfb903d
Mansura Khanom <mansura.ara@gmail.com>
2021-02-07 20:31:28 -0700

    search by first letter; created fetch-handler function

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 // Reading the seach input and showing them on the console bar
 const searchBtn = document.getElementById("searchBtn");
+const foodName = document.getElementById("food-name");
+
+// Creating fetchHandler function to reuse it multiple times
+function fetchHandler(url) {
+    const fetchData = fetch(url).then(
+        res => {
+            // Response validation and showing error message
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw "No results found!";
+            }
+        },
+        rej => { throw "No results found!"; }
+    ).then(
+        data => {
+            // data validation and showing error message
+            if (data.meals !== null) {
+                // console.log(data);
+                foodName.innerText = data.meals[0].strMeal;
+            } else {
+                throw "No results found!";
+            }
+        },
+        rej => { throw "No results found!"; }
+    ).catch(err => foodName.innerText = err);
+}
 
 searchBtn.addEventListener("click", function () {
     const searchInput = document.getElementById("search").value;
-    const foodName = document.getElementById("food-name");
 
     // Eleminating null string value
-    if (searchInput !== "") {
-        const fetchData = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`).then(
-            res => {
-                // Response validation and showing error message
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    throw "No results found!";
-                }
-            },
-            rej => { throw "No results found!"; }
-        ).then(
-            data => {
-                // data validation and showing error message
-                if (data.meals !== null) {
-                    // console.log(data);
-                    foodName.innerText = data.meals[0].strMeal;
-                } else {
-                    throw "No results found!";
-                }
-            },
-            rej => { throw "No results found!"; }
-        ).catch(err => foodName.innerText = err)
-
-    } else {
+    if (searchInput === "") {
         foodName.innerText = "Please enter a valid input!";
+    } else if (searchInput.length === 1) {
+        url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchInput}`;
+        fetchHandler(url);
+    } else {
+        url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
+        fetchHandler(url);
     }
 
 })
