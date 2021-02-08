3869982b9844767d40c1e89274185ea06710aa88
Mansura Khanom <mansura.ara@gmail.com>
2021-02-07 19:35:48 -0700

    filter an empty string; fetched API by name & printed

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-
+// Reading the seach input and showing them on the console bar
 const searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", function () {
     const searchInput = document.getElementById("search").value;
     const foodName = document.getElementById("food-name");
-    foodName.innerText = searchInput;
+
+    // Eleminating null string value
+    if (searchInput !== "") {
+        const fetchData = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`).then(
+            res => res.json()
+        ).then(
+            data => {
+                foodName.innerText = data.meals[0].strMeal;
+            }
+        )
+
+    } else {
+        foodName.innerText = "Please enter a valid input!";
+    }
 
 })
